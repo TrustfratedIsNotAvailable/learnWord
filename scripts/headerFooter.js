bf82820d5c400c5167ee2de9cabb970dbb8cdbd5
TrustfratedIsNotAvailable <nusratmithila524@gmail.com>
2025-03-15 21:49:27 +0600

    login-logout process added

--- scripts/headerFooter.js
+++ scripts/headerFooter.js
@@ -10,15 +10,5 @@
             });
     }
 
-    //index.html: Load the header and footer dynamically
     loadHTML('header.html', 'header-placeholder');
     loadHTML('footer.html', 'footer-placeholder');
-
-    //learn.html:
-    loadHTML('header.html', 'header-placeholder-learn');
-    loadHTML('footer.html', 'footer-placeholder-learn');
-
-    //FAQ.html
-    loadHTML('header.html', 'header-placeholder-FAQ');
-    loadHTML('footer.html', 'footer-placeholder-FAQ');
-
---
+++ scripts/loginOut.js
@@ -0,0 +1,41 @@
+const login = () =>{
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+    
+        if (username === "") {
+            Swal.fire({
+                position: "top",
+                title: "Please enter your name!!",
+              });
+            return;
+        }
+    
+        if (password === "123456") {
+            Swal.fire({
+                title: "Good job!",
+                text: "You logged in successfully!",
+                icon: "success"
+            });
+            document.getElementById('username').value = "";
+            document.getElementById('password').value = "";
+
+            document.getElementById('banner').classList.add("hidden");
+            document.getElementById('learn').classList.remove("hidden");
+            document.getElementById('FAQ').classList.remove("hidden");
+            document.getElementById('menu').classList.remove("hidden");
+        }
+        else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...Incorrect Password",
+              });
+              document.getElementById('password').value = "";
+        }
+}
+
+const logOut = () =>{
+    document.getElementById('banner').classList.remove("hidden");
+    document.getElementById('learn').classList.add("hidden");
+    document.getElementById('FAQ').classList.add("hidden");
+    document.getElementById('menu').classList.add("hidden");
+}
